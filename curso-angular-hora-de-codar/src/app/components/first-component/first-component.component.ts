56d1c9c5a7eac52e999c2fd55b3603b1d88e9f2c
victor-pagnozi <victor.pagnozi@gmail.com>
2022-05-07 19:14:45 -0300

    Created examples of the interpolation

--- curso-angular-hora-de-codar/src/app/components/first-component/first-component.component.ts
+++ curso-angular-hora-de-codar/src/app/components/first-component/first-component.component.ts
@@ -3,13 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-first-component',
   templateUrl: './first-component.component.html',
-  styleUrls: ['./first-component.component.css']
+  styleUrls: ['./first-component.component.css'],
 })
 export class FirstComponentComponent implements OnInit {
+  name: string = 'Victor';
+  age: number = 25;
+  job: string[] = ['Programador', 'Designer'];
 
-  constructor() { }
+  car = {
+    name: 'Civic',
+    year: 2021
+  };
 
-  ngOnInit(): void {
-  }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
