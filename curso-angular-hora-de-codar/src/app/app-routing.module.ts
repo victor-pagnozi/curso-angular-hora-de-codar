fb749ad148f4cf4cec70bc0ce6a0ed04e7ce4193
victor-pagnozi <victor.pagnozi@gmail.com>
2022-05-28 17:06:31 -0300

    Created routes and separate some components in some files

--- curso-angular-hora-de-codar/src/app/app-routing.module.ts
+++ curso-angular-hora-de-codar/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FirstComponentComponent } from './components/first-component/first-component.component';
+import { IfRenderComponent } from './components/if-render/if-render.component';
+import { ListRenderComponent } from './components/list-render/list-render.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: FirstComponentComponent },
+  { path: 'list', component: ListRenderComponent },
+  { path: 'if-render', component: IfRenderComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
